012798d9ef25b7d8817b9ee909bc244ed2bdacc6
Ricardo Ferreira <ricardoferreirades@gmail.com>
2025-09-15 20:45:58 +0200

    feat: update AppState to support authentication
    - Add UserRepository generic parameter to AppState
    - Include AuthService in application state
    - Update constructor to accept auth service and user repository
    - Enable authentication functionality throughout the application

--- src/presentation/handlers/app_state.rs
+++ src/presentation/handlers/app_state.rs
@@ -1,24 +1,29 @@
 use crate::application::ShortenUrlUseCase;
-use crate::domain::repositories::UrlRepository;
+use crate::domain::repositories::{UrlRepository, UserRepository};
+use crate::domain::services::AuthService;
 
 /// Application state that contains both use cases and repositories
 #[derive(Clone)]
-pub struct AppState<R>
+pub struct AppState<R, U>
 where
     R: UrlRepository + Send + Sync + Clone,
+    U: UserRepository + Send + Sync + Clone,
 {
     pub shorten_url_use_case: ShortenUrlUseCase<R>,
     pub url_repository: R,
+    pub auth_service: AuthService<U>,
 }
 
-impl<R> AppState<R>
+impl<R, U> AppState<R, U>
 where
     R: UrlRepository + Send + Sync + Clone,
+    U: UserRepository + Send + Sync + Clone,
 {
-    pub fn new(shorten_url_use_case: ShortenUrlUseCase<R>, url_repository: R) -> Self {
+    pub fn new(shorten_url_use_case: ShortenUrlUseCase<R>, url_repository: R, auth_service: AuthService<U>) -> Self {
         Self {
             shorten_url_use_case,
             url_repository,
+            auth_service,
         }
     }
 }
