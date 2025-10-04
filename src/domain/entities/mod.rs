a34f87f88170ff8f3b2ee18829a4532a7a572011
Ricardo Ferreira <ricardoferreirades@gmail.com>
2025-10-04 20:25:07 +0200

    feat(domain): export password reset token entity
    - Add PasswordResetToken to public exports
    - Enable external access to token entity

--- src/domain/entities/mod.rs
+++ src/domain/entities/mod.rs
@@ -2,8 +2,10 @@ pub mod url;
 pub mod short_code;
 pub mod user;
 pub mod click;
+pub mod password_reset_token;
 
 pub use url::{Url, UrlStatus};
 pub use short_code::{ShortCode, ShortCodeError};
 pub use user::{User, ProfilePrivacy};
 pub use click::Click;
+pub use password_reset_token::PasswordResetToken;
