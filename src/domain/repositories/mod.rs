dff2dd41d81f704b4ab872b92cc3731f01d8385a
Ricardo Ferreira <ricardoferreirades@gmail.com>
2025-10-04 20:31:59 +0200

    feat(domain): export password reset repository
    - Add PasswordResetRepository to public exports
    - Add PasswordResetRepositoryError to public exports
    - Enable access to password reset repository interface

--- src/domain/repositories/mod.rs
+++ src/domain/repositories/mod.rs
@@ -1,7 +1,9 @@
 pub mod url_repository;
 pub mod user_repository;
 pub mod click_repository;
+pub mod password_reset_repository;
 
 pub use url_repository::{UrlRepository, RepositoryError, UrlStats};
 pub use user_repository::{UserRepository, RepositoryError as UserRepositoryError};
 pub use click_repository::{ClickRepository, ClickStats, RepositoryError as ClickRepositoryError};
+pub use password_reset_repository::{PasswordResetRepository, PasswordResetRepositoryError};
