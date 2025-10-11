4805d05aa68b5d152d4911227386da072b3ddb06
Ricardo Ferreira <ricardoferreirades@gmail.com>
2025-10-11 15:33:14 +0200

    feat(entity): register AccountDeletionToken in entities module
    Added account_deletion_token module and public export to entities.

--- src/domain/entities/mod.rs
+++ src/domain/entities/mod.rs
@@ -3,9 +3,11 @@ pub mod short_code;
 pub mod user;
 pub mod click;
 pub mod password_reset_token;
+pub mod account_deletion_token;
 
 pub use url::{Url, UrlStatus};
 pub use short_code::{ShortCode, ShortCodeError};
 pub use user::{User, ProfilePrivacy};
 pub use click::Click;
 pub use password_reset_token::PasswordResetToken;
+pub use account_deletion_token::AccountDeletionToken;
